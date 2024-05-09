c1eeecef050a1c7f39c1abddf172cef292299dda
GustavoUchoa <dev@gustavouchoa.net>
2024-05-09 18:56:54 -0300

    Add banco de dados(migrations) e contexto via DI

--- ICI.ProvaCandidato.Dados/Context/NoticiasContext.cs
+++ ICI.ProvaCandidato.Dados/Context/NoticiasContext.cs
@@ -5,6 +5,8 @@ namespace ICI.ProvaCandidato.Dados.Context
 {
     public class NoticiasContext : DbContext
     {
+        public NoticiasContext(DbContextOptions<NoticiasContext> options) : base(options) { }
+
         public DbSet<Tag> Tags => Set<Tag>();
         public DbSet<Noticia> Noticias => Set<Noticia>();
         public DbSet<NoticiaTag> NoticiaTags => Set<NoticiaTag>();
